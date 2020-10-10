df6649d16dba647c4e12b960da849b735979c701
Fraberg <frcs.berger@gmail.com>
2020-10-10 02:51:45 +0200

    local

---
+++ project/v2/app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+//= require underscore
+//= require backbone
+//= require yt_larry_scott
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
